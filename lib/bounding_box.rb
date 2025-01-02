2c055d2e09c6e8717d5df7f6ef45bc8f291fd441
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-01-02 18:10:17 +0000

    Reduce maximum request area limit to 600 in settings configuration and improve error message for bounding box size validation

--- lib/bounding_box.rb
+++ lib/bounding_box.rb
@@ -73,7 +73,7 @@ class BoundingBox
     # check the bbox isn't too large
     if area > max_area
       raise OSM::APIBadBoundingBox, "The maximum bbox size is #{max_area}" \
-                                    ", and your request was too large. Either request a smaller area, or use planet.osm"
+                                    ", and your request of #{area} was too large. Either request a smaller area, or use planet.osm"
     end
     self
   end
